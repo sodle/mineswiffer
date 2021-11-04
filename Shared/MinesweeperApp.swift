26891cc7e588bb865d08767794870e61e8ec1d02
Scott Odle <scott@sjodle.com>
2021-11-04 09:01:48 -0700

    Initial Commit

---
+++ Shared/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Shared
+//
+//  Created by Scott Odle on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
